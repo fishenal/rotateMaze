0bb081d05f1c6c4d3fc0ce4a427a042fa98339b7
fishenal <yu_dong_han@hotmail.com>
2019-08-12 17:18:17 +0800

    feature: rotate, handle click

--- src/game.js
+++ src/game.js
@@ -4,6 +4,15 @@ export const gameData = [
     [[3,0], [3,1], [3,2]],
     [[0,1], [0,2], [0,3]],
 ]
+const $ = (selector, el) => {
+    if (!el) {el = document;}
+    return el.querySelectorAll(selector);
+}
+
+const queryBlock = (rowIdx, lineIdx) => {
+    const nodeList = $(`.block[rowIdx="${rowIdx}"][lineIdx="${lineIdx}"]`);
+    return nodeList[0];
+}
 
 export const returnLineDiv = (pos) => {
     return ['<div class="', `line-${pos}`, '"></div>'].join('');
@@ -13,31 +22,58 @@ export const returnBlockDiv = (lineSet, {rowIdx, lineIdx}) => {
     const lines = lineSet.map((pos) => (
         returnLineDiv(pos)
     ))
-    return `<div class="block" rowIdx="${rowIdx}" lineIdx="${lineIdx}">${lines.join('')}</div>`;
+    return `<td class="block" rowIdx="${rowIdx}" lineIdx="${lineIdx}">${lines.join('')}</td>`;
 }
 
 export const returnBoardDiv = (gameData) => {
-    const gameHtml = gameData.map((line, lineIdx) => (
-        line.map((row, rowIdx) => (
+    const gameHtml = gameData.map((line, lineIdx) => {
+        const trHtml = line.map((row, rowIdx) => (
             returnBlockDiv(row, {rowIdx, lineIdx})
         )).join('')
-    )).join('')
+        return `<tr>${trHtml}</tr>`
+    }).join('')
 
-    return `<div class="board">${gameHtml}</div>`;
+    return `<table class="board">${gameHtml}</table>`;
+}
+
+const rotate = (set) => {
+    const ro = (pos) => {
+        if (pos === 0) {
+            return 3
+        }
+        return pos - 1
+    }
+    const result = [ro(set[0]), ro(set[1])];
+    return result;
 }
 
 export class Game {
     constructor(gameData) {
         this.gameData = gameData;
+        this.root = document.getElementById('root');
+    }
+
+    handleClick(e) {
+        if (e.target.tagName !== 'TD' || e.target.className !== 'block') {
+            return;
+        }
+        const rowIdx = Number(e.target.getAttribute('rowIdx'));
+        const lineIdx = Number(e.target.getAttribute('lineIdx'));
+        console.log(this.gameData)
+        console.log(rowIdx, lineIdx)
+        this.gameData[lineIdx][rowIdx] = rotate(this.gameData[lineIdx][rowIdx]);
+        this.render()
+        // const block = queryBlock(rowIdx, lineIdx);
+
     }
 
     render() {
-        console.log(this.gameData)
         const gameHtml = returnBoardDiv(this.gameData);
-        console.log(typeof gameHtml)
-        document.getElementById('root').innerHTML = gameHtml;
+        this.root.innerHTML = gameHtml;
+        this.root.onclick = this.handleClick.bind(this);
         // const htmlStr = this.gameData
     }
 }
 
-window.Game = Game;
+const game = new Game(gameData);
+game.render();
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/game.js',
   output: {
     filename: 'game.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    devtoolLineToLine: true
   }
 };
